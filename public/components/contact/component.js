2ba0f09c5484416ed9de018dc1af0e13ea1115cb
Ryan Farrell <ryan@cartoonfood.com>
2018-10-05 00:32:58 -0700

    Adds "work" to the comment on the contact component controller

--- public/components/contact/component.js
+++ public/components/contact/component.js
@@ -3,7 +3,7 @@ angular
 .component('contact', {
     controller: 'ContactController',
     templateUrl: '/components/contact/template.html',
-    bindToController: true //required for $scope to work
+    bindToController: true //required for $scope to 
 })
 .controller('ContactController',function($scope, UserFactory){
     $scope.user = UserFactory;
